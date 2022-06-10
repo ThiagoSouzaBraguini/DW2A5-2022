143bd5bfbcc62837d33e839b8cbd7a533f6c3370
Thiago de Souza <thiagoszbraguini@hotmail.com>
2022-06-10 01:34:16 -0300

    Aula 30

--- hdcevents/storage/framework/views/9c4bdd552dad8d352eada02173aa9f20107d6d99.php
+++ hdcevents/storage/framework/views/9c4bdd552dad8d352eada02173aa9f20107d6d99.php
@@ -4,25 +4,35 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
+    <form action="/" method = "GET">
         <input type="text" name="search" id="search" class= "form-control" placeholder ="Procurar...">
     </form>
 </div>
 <div id="envents-container" class="cold-md-12">
+    <?php if($search): ?>
+    <h2>Buscando por <?php echo e($search); ?></h2>
+    <?php else: ?>
     <h2>Próximos Eventos</h2>
     <p class ="subtitle">Veja os eventos dos próximos dias</p>
+    <?php endif; ?>
+    
     <div id="cards-container" class="row">
         <?php $__currentLoopData = $events; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $event): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
         <div class="card col-md-3">
             <img src="/img/events/<?php echo e($event->image); ?>" alt="<?php echo e($event->title); ?>">
             <div class="card-body">
-                <p class="card-date">27/05/2022</p>
+                <p class="card-date"><?php echo e(date('d/m/Y',strtotime($event->date))); ?></p>
                 <h5 class="card-title"><?php echo e($event->title); ?></h5>
-                <p class="card-participants">X participantes</p>
-                <a href="#" class= "btn btn-primary">Saber mais</a>
+                <p class="card-participants"><?php echo e(count($event->users)); ?> participantes</p>
+                <a href="/events/<?php echo e($event->id); ?>" class= "btn btn-primary">Saber mais</a>
             </div>
         </div>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        <?php if(count($events) == 0 && $search): ?>
+            <p>Não foi possível encontrar nenhum evento com <?php echo e($search); ?>! <a href="/">Ver todos</a></p>
+        <?php elseif(count($events) == 0): ?>
+        <p>Não há eventos disponíveis</p>
+        <?php endif; ?>
     </div>
 </div>
 
--- hdcevents/storage/framework/views/ec6053870af2ebf5b38c9ed2015d0f74ab3a090b.php
+++ hdcevents/storage/framework/views/ec6053870af2ebf5b38c9ed2015d0f74ab3a090b.php
@@ -33,12 +33,31 @@
                     <li class="nav-item">
                         <a href="/events/create" class="nav-link">Criar Eventos</a>
                     </li>
+                    <?php if(auth()->guard()->check()): ?>
                     <li class="nav-item">
-                        <a href="/" class="nav-link">Entrar</a>
+                        <a href="/dashboard" class="nav-link">Meus eventos</a>
                     </li>
                     <li class="nav-item">
-                        <a href="/" class="nav-link">Cadastrar</a>
+                        <form action="logout" method = "POST">
+                            <?php echo csrf_field(); ?>
+                            <a 
+                                href="/logout" 
+                                class="nav-link" 
+                                onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                                Sair
+                            </a>
+                        </form>
                     </li>
+                    <?php endif; ?>
+                    <?php if(auth()->guard()->guest()): ?>
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/register" class="nav-link">Cadastrar</a>
+                    </li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </nav>
